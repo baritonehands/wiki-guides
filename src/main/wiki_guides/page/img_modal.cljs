6ca826a65fbefcc58841b210b65070282f866ca6
brian <>
2023-12-31 06:59:04 +0000

    Improved image sizes
    FossilOrigin-Name: ffcab54946ec63f7d9690ca02af85d61f1d2ad1d4f087cc258a65ddde7c74471

--- src/main/wiki_guides/page/img_modal.cljs
+++ src/main/wiki_guides/page/img_modal.cljs
@@ -1,5 +1,5 @@
 (ns wiki-guides.page.img-modal
-  (:require [re-com.core :refer [modal-panel]]
+  (:require [re-com.core :refer [md-icon-button modal-panel]]
             [reagent.core :as r]))
 
 (defonce *url (r/atom nil))
@@ -13,7 +13,13 @@
 (defn view []
   (if @*url
     [modal-panel
+     :backdrop-opacity 0.8
      :backdrop-on-click hide!
      :wrap-nicely? false
      :child
-     [:img {:src @*url :style {:max-width "80vw"}}]]))
+     [:div.img-modal
+      [md-icon-button
+       :md-icon-name "zmdi-close"
+       :size :larger
+       :on-click hide!]
+      [:img {:src @*url :style {:max-width "80vw"}}]]]))
